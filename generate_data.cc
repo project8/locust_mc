75658c19e7e900915686fb9d70ffff3b530e05bc
Gray Rybka <grybka@uw.edu>
2013-04-18 23:05:14 -0700

    added some receiver shape and cleaned things up a bit

--- generate_data.cc
+++ generate_data.cc
@@ -6,21 +6,24 @@
 #include <iostream>
 #include <string.h>
 #include <yajl/yajl_gen.h>
+#include <yajl/yajl_tree.h>
 using namespace std;
 
 //program constants
-//unsigned char mcname[]="scarab_mc";
-char mcname[]="scarab_mc";
-//ipunsigned char token_mc_id[]="mc_id";
+char mcname[]="locust_mc";
 
 //physical constants
 float kB=1.3806e-23;  // Boltzman constant W/K
 float c=3e10; //speed of light in cm/s
 double ecyclo=1.758820e11; //electron cyclotron frequency in rad/s
 double emass=510998.9; //electron mass in eV
+double JoulesToEv=1/1.6e-19; // eV/J
 
+// Waveguide is set up like this
 // --<A|---<B>-----<C>----|D>-----
 // ------------lwg--------------dl
+// Where A,B,C,D are noise sources
+// lwg and dl are the waveguide length and extra cable length
 
 float c_wg=c*sqrt(1-pow(c/(2.0*0.42*2.54*26e9),2.0)); //speed of light in waveguide in cm/s
 float c_cable=0.6*c; //speed of light in cables in cm/s (this is a guess)
@@ -29,10 +32,8 @@ float dl=5; //extra length on channel 2 cable in cm
 int record_size=4194304; //number of samples in record time series
 int fft_size=record_size/2+1;
 float digitizer_fullscale=0.5; //in volts
-//float amp_temp=0;
-float amp_temp=30;
-//float uncor_temp=0;
-float uncor_temp=30;
+float amp_temp=30; //in Kelvin
+float uncor_temp=30; //in Kelvin
 float T_A=uncor_temp; //temperature of noise source A
 float T_B=amp_temp; //temperature of noise source B
 float T_C=amp_temp; //temperature of noise source C
@@ -48,41 +49,59 @@ fftwf_complex *signal_f; //signal without noise frequency space
 float *channel1; //channel 1 record in time space
 float *channel2; //channel 2 record in time space
 float *signal_t; //signal without noise in time space
-fftwf_plan channel1_plan;
-fftwf_plan channel2_plan;
-fftwf_plan signal_plan;
+fftwf_plan channel1_plan; //for fftw
+fftwf_plan channel2_plan; //for fftw
+fftwf_plan signal_plan; //for fftw
 unsigned fft_flags=FFTW_ESTIMATE;
 
-float total_mixing_frequency=25e9; //sum of local oscillator freqencies, in Hz
+float hf_mixing_frequency=24.2e9; //high frequency oscillator in Hz
+float lf_mixing_frequency=500e6;  //low frequency oscillator in Hz
+float total_mixing_frequency=hf_mixing_frequency+lf_mixing_frequency; //sum of local oscillator freqencies, in Hz
 float sampling_rate=200e6; //digitizer sampling rate in Hz
-float frequency_bin_width=sampling_rate/((float)record_size);
+float frequency_bin_width=sampling_rate/((float)record_size); //how big one bin is, in Hz
 
-int nrecords=2; //how many records to generate
+int nrecords=10; //how many records to generate
+double expected_event_rate=100;  //expected event rate in hz
 
 string eggname="test.egg"; //output egg file name
 string mcinfoname="test.mcinfo"; //output mcinfo name
 
+//transfer function
+float *hf_frequencies; //frequencies used for transfer function
+float *hf_full_transferfunction_ch1; //transfer function NOT IN DB
+float *hf_full_transferfunction_ch2;
+int hf_arraycount; //size of big transfer function arrays
+float *hf_transferfunction_ch1; //high frequency transfer function interpolated into current band
+float *hf_transferfunction_ch2; //high frequency transfer function interpolated into current band
+
 class ChirpEvent {
 public:
-    double start_energy; //in eV
     double duration; //in seconds
     double start_time; //in seconds
+    double start_frequency; //in Hz
+    double dfdt; //in Hz/s
+    double power; //power radiated into chirp in Watts
+    //not actually used, but relevant to calculating above parameters
+    double start_energy; //in eV
 };
 
-ChirpEvent *events;
-int nevents=0;
+ChirpEvent *events; //array of events to add
+int nevents=0; //how many events are there
 double on_time=0; //progress in generation in seconds
 
-double record_time=((double)record_size)/sampling_rate;
+double record_time=((double)record_size)/sampling_rate; //length of one record in seconds
 
 float getGaussianRand(float mean,float sigma);
 float getUniformRand(float range);
+float getExponentialRand(float scale);
 void generate_record(unsigned char *dataptr);
 void make_json_string_entry(yajl_gen gen,const char *key,const char *value);
 void make_json_numeric_entry(yajl_gen gen,const char *key,double value);
 void make_json_integer_entry(yajl_gen gen,const char *key,int value);
 void yajl_gen_number_forreals(yajl_gen gen,double num);
 void yajl_gen_integer_forreals(yajl_gen gen,int num);
+int load_transfer_functions(const char *fname);
+float interpolate_transferfunction(float *freqs,float *vals,int len,float f);
 
 int main(int argc,char *argv[])
 {
@@ -94,12 +113,17 @@ int main(int argc,char *argv[])
     channel1_f=(fftwf_complex*)fftwf_malloc(sizeof(fftwf_complex)*fft_size);
     channel2_f=(fftwf_complex*)fftwf_malloc(sizeof(fftwf_complex)*fft_size);
     signal_f=(fftwf_complex*)fftwf_malloc(sizeof(fftwf_complex)*fft_size);
+    hf_transferfunction_ch1=new float[fft_size];
+    hf_transferfunction_ch2=new float[fft_size];
     channel1=(float*)fftwf_malloc(sizeof(float)*record_size);
     channel2=(float*)fftwf_malloc(sizeof(float)*record_size);
     signal_t=(float*)fftwf_malloc(sizeof(float)*record_size);
     channel1_plan=fftwf_plan_dft_c2r_1d(record_size,channel1_f,channel1,fft_flags);
     channel2_plan=fftwf_plan_dft_c2r_1d(record_size,channel2_f,channel2,fft_flags);
     signal_plan=fftwf_plan_dft_r2c_1d(record_size,signal_t,signal_f,fft_flags);
+    //Load Receiver Transfer Function
+    load_transfer_functions("receiver_transfer_functions.json");
+    //
     //Prep egg file
     Monarch *egg=Monarch::OpenForWriting(eggname.c_str());
     MonarchHeader *header=egg->GetHeader();
@@ -112,24 +136,30 @@ int main(int argc,char *argv[])
     }
     //prep mcinfo file
     yajl_gen mcinfo_json=yajl_gen_alloc(NULL);
+    yajl_gen_config(mcinfo_json, yajl_gen_beautify, 1);
     yajl_gen_map_open(mcinfo_json);
     make_json_string_entry(mcinfo_json,"mc_id",mcname);
-    make_json_numeric_entry(mcinfo_json,"system_temperature",20.4);
-    make_json_string_entry(mcinfo_json,"noise_model","full_receiver_model");
+    make_json_numeric_entry(mcinfo_json,"system_temperature",90);
+    make_json_string_entry(mcinfo_json,"noise_model","full_receiver_model_minus_lf_mixing");
     make_json_string_entry(mcinfo_json,"receiver_assumptions","TODO");
+    make_json_numeric_entry(mcinfo_json,"lf_mixing_frequency",lf_mixing_frequency);
+    make_json_numeric_entry(mcinfo_json,"hf_mixing_frequency",hf_mixing_frequency);
 
-    //figure out events
-    nevents=0;
-//    events=new ChirpEvent[nevents];
+    //place some events
+    nevents=5; //TODO this should be in a config file or something
+    events=new ChirpEvent[nevents];
     for(int i=0;i<nevents;i++) {
-	events[i].start_time=0;
-	events[i].duration=100;
+	events[i].start_time=getUniformRand(record_time*((float)nrecords));
+	events[i].duration=getExponentialRand(200e-6);
 	events[i].start_energy=emass*(BField*ecyclo/((total_mixing_frequency+50e6)*2.0*M_PI)-1);
+	events[i].start_frequency=ecyclo*emass/(emass+events[i].start_energy);
+	events[i].power=1e-15;
+	events[i].dfdt=events[i].start_frequency*events[i].power*JoulesToEv/emass;
     }
 
-    //record event info
+    //record event info in json file
     yajl_gen_string(mcinfo_json,(unsigned char*)("events"),strlen("events"));
-    yajl_gen_map_open(mcinfo_json);
+    yajl_gen_array_open(mcinfo_json);
     for(int i=0;i<nevents;i++) {
 	//record support
 	int startrecord=(int)(events[i].start_time/record_time);
@@ -140,6 +170,7 @@ int main(int argc,char *argv[])
 	    endrecord=nrecords-1;
 	    endsamp=record_size-1;
 	}
+	yajl_gen_map_open(mcinfo_json);
 	yajl_gen_string(mcinfo_json,(unsigned char*)("support"),strlen("support"));
 	yajl_gen_array_open(mcinfo_json);
 	yajl_gen_integer(mcinfo_json,startrecord);
@@ -149,8 +180,13 @@ int main(int argc,char *argv[])
 	yajl_gen_array_close(mcinfo_json);
 	//record start energy
 	make_json_numeric_entry(mcinfo_json,"start_energy",events[i].start_energy);
+	make_json_numeric_entry(mcinfo_json,"start_frequency",events[i].start_frequency);
+	make_json_numeric_entry(mcinfo_json,"power",events[i].power);
+	make_json_numeric_entry(mcinfo_json,"dfdt",events[i].dfdt);
+	make_json_numeric_entry(mcinfo_json,"duration",events[i].duration);
+	yajl_gen_map_close(mcinfo_json);
     }
-    yajl_gen_map_close(mcinfo_json);
+    yajl_gen_array_close(mcinfo_json);
 
     //generate monte carlo
     for(int onrecord=0;onrecord<nrecords;onrecord++) {
@@ -191,7 +227,7 @@ int main(int argc,char *argv[])
 //assumes an interleaved data pointer
 void generate_record(unsigned char *dataptr) 
 {
-    float R=50; //cable impedence, ohms
+    float R=50; //cable impedence, ohms (so I can turn power into volts)
     float sigma_a=sqrt(R*kB*T_A/2.0);
     float sigma_b=sqrt(R*kB*T_B/2.0);
     float sigma_c=sqrt(R*kB*T_C/2.0);
@@ -206,6 +242,7 @@ void generate_record(unsigned char *dataptr)
 	Dnoise_f[k]=getGaussianRand(0,sigma_d)+I*getGaussianRand(0,sigma_d);
     }
 
+    //combine noise sources
     //Add A+B+Ce^ilwg and (D+C+Be^ilwg)e^idl
     for(int i=0;i<fft_size;i++) {
 	float f=total_mixing_frequency+frequency_bin_width*((float)i);
@@ -215,7 +252,8 @@ void generate_record(unsigned char *dataptr)
 	channel2_f[i]=Dnoise_f[i]+Cnoise_f[i]+Bnoise_f[i]*wg_e;
     }
 
-    //inject signal
+    //--inject signal--
+    //first make signal in time space
     bool signal_present=false;
     for(int i=0;i<record_size;i++) {
 	signal_t[i]=0;
@@ -223,26 +261,21 @@ void generate_record(unsigned char *dataptr)
     for(int k=0;k<nevents;k++) {
 	if(events[k].start_time<on_time||(events[k].start_time+events[k].duration)>(on_time+record_time)) { //this event is in my range
 	    signal_present=true;
-	    double frequency=ecyclo*emass/(emass+events[k].start_energy);
-	    cerr << "frequency is " << frequency << endl;
-	    //TODO include energy loss here, putting 1e-15 watts for starters
-	    double chirpiness=frequency*6250/emass;
-	    chirpiness=0;
-	    double vmax=sqrt(1e-15*R);
+	    double vmax=sqrt(events[k].power*R);
 	    long start_sample=(events[k].start_time-on_time)*sampling_rate;
 	    if(start_sample<0) start_sample=0;
 	    long stop_sample=(events[k].start_time+events[k].duration-on_time)*sampling_rate;
 	    if(stop_sample>=record_size) stop_sample=record_size-1;
-//	    cerr << "start sample " << start_sample << " stop sample " << stop_sample << "record size " << record_size << endl;
 	    double  fftnorm=1/((double)record_size);
 	    for(int i=start_sample;i<=stop_sample;i++) {
 		double t=on_time+((double)i)/sampling_rate;
 		double dt=t-events[k].start_time;
-		double phase=frequency*dt+chirpiness*dt*dt-total_mixing_frequency*dt;
+		double phase=events[k].start_frequency*dt+events[k].dfdt*dt*dt-total_mixing_frequency*dt;
 		signal_t[i]+=fftnorm*vmax*cos(phase);
 	    }
 	}
     }
+    //now turn signal into frequency space
     if(signal_present) {
 	fftwf_execute(signal_plan);
 	for(int i=0;i<fft_size;i++) {
@@ -259,10 +292,18 @@ void generate_record(unsigned char *dataptr)
 	channel2_f[i]=channel2_f[i]*cable_e;
     }
 
-    //TODO apply receiver chain transfer function
+    //apply high frequency transfer function
     for(int i=0;i<fft_size;i++) {
-	channel1_f[i]*=1e11;
-	channel2_f[i]*=1e11;
+	channel1_f[i]*=hf_transferfunction_ch1[i];
+	channel2_f[i]*=hf_transferfunction_ch2[i];
+    }
+
+    //TODO apply low frequency receiver chain transfer function
+    for(int i=0;i<fft_size;i++) {
+	//this applies uniform 70 dB gain
+	//as a stopgap measure until real gain is known
+	channel1_f[i]*=1e7;
+	channel2_f[i]*=1e7;
     }
 
     //fft to construct the time series again
@@ -274,7 +315,7 @@ void generate_record(unsigned char *dataptr)
     for(int i=0;i<record_size;i++) {
 	float x1=256*(fft_scale*channel1[i]/digitizer_fullscale+digitizer_fullscale);
 	float x2=256*(fft_scale*channel2[i]/digitizer_fullscale+digitizer_fullscale);
-	//clipping
+	//clipping to 8 bits
 	if(x1>255) x1=255;
 	if(x2>255) x2=255;
 	if(x1<0) x1=0;
@@ -282,12 +323,18 @@ void generate_record(unsigned char *dataptr)
 	dataptr[2*i]=(unsigned char)x1;
 	dataptr[2*i+1]=(unsigned char)x2;
     }
+    //update time in seconds
     on_time+=record_time;
 }
 
 float getUniformRand(float range)
 {
     return range*((float)rand())/((float)RAND_MAX);
+}
+
+float getExponentialRand(float scale)
+{
+    return -scale*log(getUniformRand(1.0));
 }
 
 bool has_spare_grand=false;
@@ -312,6 +359,8 @@ float getGaussianRand(float mean,float sigma)
 }
 
 
+//-----some json helper functions---
+//
 void make_json_string_entry(yajl_gen gen,const char *key,const char *value)
 {
     yajl_gen_string(gen,(unsigned char*)key,strlen(key));
@@ -337,4 +386,72 @@ void yajl_gen_number_forreals(yajl_gen gen,double num)
     sprintf(buffer,"%f",num);
     yajl_gen_number(gen,buffer,strlen(buffer));
 }
-    
+
+int load_transfer_functions(const char *fname)
+{
+    //I just copied this from the yajl example
+    //it loads the transfer function in json form and then interpolates around
+    //the desired bandwidth
+    FILE *file=fopen(fname,"r");
+    char errbuf[1024];
+    static unsigned char fileData[65536];
+    fileData[0] = errbuf[0] = 0;
+    size_t rd = fread((void *) fileData, 1, sizeof(fileData) - 1, file);
+    if (rd == 0 && !feof(stdin)) {
+        fprintf(stderr, "error encountered on file read\n");
+        return 1;
+    } else if (rd >= sizeof(fileData) - 1) {
+        fprintf(stderr, "config file too big\n");
+        return 1;
+    }
+    yajl_val node;
+    node = yajl_tree_parse((const char *) fileData, errbuf, sizeof(errbuf));
+    if (node == NULL) {
+        fprintf(stderr, "parse_error: ");
+        if (strlen(errbuf)) fprintf(stderr, " %s", errbuf);
+        else fprintf(stderr, "unknown error");
+        fprintf(stderr, "\n");
+        return 1;
+    }
+    const char *freqpath[]={"hf_frequencies",(const char*)0};
+    const char *hf1path[]={"hf_transfer_channel1",(const char*)0};
+    const char *hf2path[]={"hf_transfer_channel2",(const char*)0};
+    yajl_val freqs=yajl_tree_get(node,freqpath,yajl_t_array);
+    yajl_val hf1=yajl_tree_get(node,hf1path,yajl_t_array);
+    yajl_val hf2=yajl_tree_get(node,hf2path,yajl_t_array);
+    hf_frequencies=new float[freqs->u.array.len];
+    hf_full_transferfunction_ch1=new float[freqs->u.array.len];
+    hf_full_transferfunction_ch2=new float[freqs->u.array.len];
+    hf_arraycount=freqs->u.array.len;
+    for(size_t i=0;i<freqs->u.array.len;i++) {
+	hf_frequencies[i]=freqs->u.array.values[i]->u.number.d;
+	hf_full_transferfunction_ch1[i]=pow10(0.1*hf1->u.array.values[i]->u.number.d);
+	hf_full_transferfunction_ch2[i]=pow10(0.1*hf2->u.array.values[i]->u.number.d);
+    }
+    yajl_tree_free(node);
+    //now create the transfer function for my bandwidth
+    for(int i=0;i<fft_size;i++) {
+	float f=total_mixing_frequency+frequency_bin_width*((float)i);
+	hf_transferfunction_ch1[i]=interpolate_transferfunction(hf_frequencies,hf_full_transferfunction_ch1,hf_arraycount,f);
+	hf_transferfunction_ch2[i]=interpolate_transferfunction(hf_frequencies,hf_full_transferfunction_ch2,hf_arraycount,f);
+    }
+    return 0;
+}
+
+//linear interpolation between frequency points
+float interpolate_transferfunction(float *freqs,float *vals,int len,float f)
+{
+    if(f<=freqs[0]) return vals[0];
+    if(f>=freqs[len-1]) return vals[len-1];
+    for(int i=0;i<len;i++) {
+	if( (f>=freqs[i])&&(f<=freqs[i+1])) {
+	    float x1=freqs[i];
+	    float x2=freqs[i+1];
+	    float y1=vals[i];
+	    float y2=vals[i+1];
+	    return y1+(y2-y1)*((f-x1)/(x2-x1));
+	}
+    }
+    cerr << "interpolation failed" << endl;
+    return nanf("bad programmer");
+}
