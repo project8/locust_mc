853a31406bedc0e5ee350b293801127f063ae8ab
Ben LaRoque <benjamin.laroque@gmail.com>
2013-08-06 13:48:17 -0700

    update python script for new setup with short

--- generate_configs.py
+++ generate_configs.py
@@ -19,12 +19,14 @@ def generate_configs(file_prefix=False, num_events=100, duration=1e-4, dfdt=1e8,
     out_dict = {
         "transfer_function_filename":
             "/home/laroque/Repos/locust_mc/receiver_transfer_functions.json",
-        "amp1_noise_temperature": 30,
+        "amp1_noise_temperature": 5,
         "amp2_noise_temperature": 30,
-        "receiver1_noise_temperature": 30,
+        "receiver1_noise_temperature": 15,
         "receiver2_noise_temperature": 30,
         "BField": 0.9,
-        "waveguide_length": 20,
+        "waveguide_length": 147.19,
+        "waveguide_setup": "SINGLEAMP",
+        "distance_to_short": 5,
         "phase_delay_length": 5,
         "hf_mixing_frequency": 24.2e9,
         "lf_mixing_frequency": 500e6,
@@ -47,10 +49,10 @@ def generate_configs(file_prefix=False, num_events=100, duration=1e-4, dfdt=1e8,
     json.dump(out_dict, outfile, indent=4)
 
 if __name__ == '__main__':
-    durrations = [200e-6, 100e-6, 50e-6]
-    powers = [1e-15, 8e-16, 6e-16, 4e-16, 2e-16, 1e-16]
+    durrations = [200e-6, 100e-6, 50e-6, 25e-6, 5e-6]
+    powers = [1e-15, 7e-16, 4e-16, 1e-16, 7e-17, 4e-17, 1e-17, 7e-18, 4e-18, 1e-18, 7e-19, 4e-19, 1e-19]
     chrates = [1e8, 3e8]
     for chrate in chrates:
         for dur in durrations:
             for powr in powers:
-                generate_configs(num_events=1000, duration=dur, dfdt=chrate, power=powr)
+                generate_configs(num_events=500, duration=dur, dfdt=chrate, power=powr)
