46cb3de2b3a8b03b96a09b0b221157f61406e8fc
Penny Slocum <penny.slocum@yale.edu>
2019-02-08 19:03:32 -0500

    Modified plane wave test in LMCPatchSignalGenerator.cc

--- Source/Generators/LMCPatchSignalGenerator.cc
+++ Source/Generators/LMCPatchSignalGenerator.cc
@@ -167,7 +167,7 @@ namespace locust
 
     double GetVoltageAmpFromPlaneWave()
     {
-        double AntennaFactor = 1./420.;
+        double AntennaFactor = 1./400.;
 
         // S = epsilon0 c E0^2 / 2.  // power/area
         //  0.6e-21 W/Hz * 24.e3 Hz / (0.00375*0.002916) = S = 1.3e-12 W/m^2
@@ -218,9 +218,11 @@ namespace locust
             VoltageAmplitude *= aPowerCombiner.GetVoltageDamping(njunctions);
         }
 
-        //if (VoltageAmplitude>0.) {printf("voltageamplitude is %g\n", VoltageAmplitude); getchar();}
+	//        if (VoltageAmplitude>0.) {printf("voltageamplitude is %g\n", VoltageAmplitude); getchar();}
         aSignal->LongSignalTimeComplex()[channelindex][0] += VoltageAmplitude * cos(VoltagePhase - phi_LO);
         aSignal->LongSignalTimeComplex()[channelindex][1] += VoltageAmplitude * sin(VoltagePhase - phi_LO);
+	//        if (VoltageAmplitude>0.) {printf("summedvoltageamplitude is %g\n", aSignal->LongSignalTimeComplex()[channelindex][0]); getchar();}                           
+
 
     }
 
@@ -332,8 +334,9 @@ namespace locust
                     //printf("tDopplerFrequency is %g\n", tDopplerFrequency); getchar();
                 }      
 
-                double tVoltageAmplitude = GetVoltageAmplitude(tCurrentParticle.CalculateElectricField(currentPatch->GetPosition()), tCurrentParticle.CalculateElectricField(currentPatch->GetPosition()).Cross(tCurrentParticle.CalculateMagneticField(currentPatch->GetPosition())), PatchPhi, tDopplerFrequency);
-                //printf("tVoltageAmplitude is %g\n", tVoltageAmplitude); getchar();
+		// double tVoltageAmplitude = GetVoltageAmplitude(tCurrentParticle.CalculateElectricField(currentPatch->GetPosition()), tCurrentParticle.CalculateElectricField(currentPatch->GetPosition()).Cross(tCurrentParticle.CalculateMagneticField(currentPatch->GetPosition())), PatchPhi, tDopplerFrequency);
+                double tVoltageAmplitude = GetVoltageAmpFromPlaneWave();
+		//                printf("tVoltageAmplitude is %g\n", tVoltageAmplitude); getchar();
 
                 AddOnePatchVoltageToStripSum(aSignal, tVoltageAmplitude, VoltagePhase_t[channelIndex*fNPatchesPerStrip+patchIndex], phiLO_t, sampleIndex, patchIndex, tDopplerFrequency);
 
