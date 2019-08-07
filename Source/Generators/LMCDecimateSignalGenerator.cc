d09f22d044520039b9287f15c1fd24f83b511fb3
arinabykadorova <arina.bykadorova@yale.edu>
2019-08-07 14:11:10 -0400

    Cleaned up so I can incorporate changes from develop.

--- Source/Generators/LMCDecimateSignalGenerator.cc
+++ Source/Generators/LMCDecimateSignalGenerator.cc
@@ -14,59 +14,69 @@ using std::string;
 
 namespace locust
 {
-    LOGGER( lmclog, "DecimateSignalGenerator" );
+  LOGGER( lmclog, "DecimateSignalGenerator" );
 
-    MT_REGISTER_GENERATOR(DecimateSignalGenerator, "decimate-signal");
+  MT_REGISTER_GENERATOR(DecimateSignalGenerator, "decimate-signal");
 
-    DecimateSignalGenerator::DecimateSignalGenerator( const std::string& aName ) :
-        Generator( aName ),
-        fDoGenerateFunc( &DecimateSignalGenerator::DoGenerateTime )
-    {
-        fRequiredSignalState = Signal::kTime;
-    }
+  DecimateSignalGenerator::DecimateSignalGenerator( const std::string& aName ) :
+    Generator( aName ),
+    fDoGenerateFunc( &DecimateSignalGenerator::DoGenerateTime )
+  {
+    fRequiredSignalState = Signal::kTime;
+  }
 
-    DecimateSignalGenerator::~DecimateSignalGenerator()
-    {
-    }
+  DecimateSignalGenerator::~DecimateSignalGenerator()
+  {
+  }
 
-    bool DecimateSignalGenerator::Configure( const scarab::param_node* aParam )
-    {
-        if( aParam == NULL) return true;
-        return true;
-    }
+  bool DecimateSignalGenerator::Configure( const scarab::param_node* aParam )
+  {
+    if( aParam == NULL) return true;
+    return true;
+  }
 
-    void DecimateSignalGenerator::Accept( GeneratorVisitor* aVisitor ) const
-    {
-        aVisitor->Visit( this );
-        return;
-    }
+  void DecimateSignalGenerator::Accept( GeneratorVisitor* aVisitor ) const
+  {
+    aVisitor->Visit( this );
+    return;
+  }
 
 
-    bool DecimateSignalGenerator::DoGenerate( Signal* aSignal )
-    {
-        return (this->*fDoGenerateFunc)( aSignal );
-    }
+  bool DecimateSignalGenerator::DoGenerate( Signal* aSignal )
+  {
+    return (this->*fDoGenerateFunc)( aSignal );
+  }
 
-    bool DecimateSignalGenerator::DoGenerateTime( Signal* aSignal ) 
-    {
-        const unsigned nchannels = fNChannels;
+  bool DecimateSignalGenerator::DoGenerateTime( Signal* aSignal ) 
+  {
+    const unsigned nchannels = fNChannels;
 
-        // Decimate Fs -> Fs/10
-        for (int ch=0; ch<nchannels; ch++)
-        {
-            for( unsigned index = 0; index < aSignal->TimeSize()*aSignal->DecimationFactor(); ++index )
-            {
-                if (index % aSignal->DecimationFactor() == 0)
-                {
-                    aSignal->SignalTimeComplex()[ch*aSignal->TimeSize() + index/aSignal->DecimationFactor()][0] =
-                        aSignal->LongSignalTimeComplex()[ch*aSignal->TimeSize()*aSignal->DecimationFactor() + index][0];
-                    aSignal->SignalTimeComplex()[ch*aSignal->TimeSize() + index/aSignal->DecimationFactor()][1] =
-                        aSignal->LongSignalTimeComplex()[ch*aSignal->TimeSize()*aSignal->DecimationFactor() + index][1];
-                }
-            }
-        }
-
-        return true;
-    }
+    //text file for VI for testing.
+    std::ofstream voltagefile;
+    voltagefile.open("voltagefile.txt");
+	
+    // Decimate Fs -> Fs/10
+    for (int ch=0; ch<nchannels; ch++)
+      {
+	for( unsigned index = 0; index < aSignal->TimeSize()*aSignal->DecimationFactor(); ++index )
+	  {
+	    if (index % aSignal->DecimationFactor() == 0)
+	      {
+		aSignal->SignalTimeComplex()[ch*aSignal->TimeSize() + index/aSignal->DecimationFactor()][0] =
+		  aSignal->LongSignalTimeComplex()[ch*aSignal->TimeSize()*aSignal->DecimationFactor() + index][0];
+		aSignal->SignalTimeComplex()[ch*aSignal->TimeSize() + index/aSignal->DecimationFactor()][1] =
+		  aSignal->LongSignalTimeComplex()[ch*aSignal->TimeSize()*aSignal->DecimationFactor() + index][1];	
+	      }
+	    	voltagefile << index;
+		voltagefile << "\n";
+		voltagefile << aSignal->LongSignalTimeComplex()[index][0];
+		voltagefile << "\n";
+		voltagefile << aSignal->LongSignalTimeComplex()[index][1];
+		voltagefile << "\n";
+	  }
+      }
+    voltagefile.close();
+    return true;
+  }
 
 } /* namespace locust */
