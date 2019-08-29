912a44d9ad8eb514201cb7c55a6c597f4766540b
Noah Oblath <nsoblath@gmail.com>
2019-08-28 17:10:57 -0700

    Cleaning out FieldCalculator

--- Source/Kassiopeia/LMCFieldCalculator.hh
+++ Source/Kassiopeia/LMCFieldCalculator.hh
@@ -17,28 +17,21 @@
 using std::vector;
 
 
-
-
-
-using katrin::KMathBracketingSolver;
+namespace Kassiopeia
+{
+    class KSParticle;
+}
 
 namespace locust
 {
 
-    class FieldCalculator :
-        public Kassiopeia::KSComponentTemplate< FieldCalculator, Kassiopeia::KSSpaceInteraction >
+    class FieldCalculator
     {
         public:
             FieldCalculator();
             FieldCalculator( const FieldCalculator& aCopy );
             FieldCalculator* Clone() const;
             ~FieldCalculator();
-
-            //*****************
-            //space interaction
-            //*****************
-
-
 
             double GetGroupVelocityTM01(Kassiopeia::KSParticle& aFinalParticle);
             double GetGroupVelocityTE10(Kassiopeia::KSParticle& aFinalParticle);
@@ -48,47 +41,6 @@ namespace locust
             double GetCouplingFactorTE10(Kassiopeia::KSParticle& aFinalParticle);
             double GetTM01FieldWithTerminator(Kassiopeia::KSParticle& aFinalParticle);
             double GetTE10FieldAfterOneBounce(Kassiopeia::KSParticle& aFinalParticle);
-
-
-
-        public:
-            void CalculateInteraction( const Kassiopeia::KSTrajectory& aTrajectory, const Kassiopeia::KSParticle& aTrajectoryInitialParticle, const Kassiopeia::KSParticle& aTrajectoryFinalParticle, const KThreeVector& aTrajectoryCenter, const double& aTrajectoryRadius, const double& aTrajectoryTimeStep, Kassiopeia::KSParticle& anInteractionParticle, double& aTimeStep, bool& aFlag );
-            void ExecuteInteraction( const Kassiopeia::KSParticle& anInteractionParticle, Kassiopeia::KSParticle& aFinalParticle, Kassiopeia::KSParticleQueue& aSecondaries ) const;
-
-            //***********
-            //composition
-            //***********
-
-        public:
-            void AddSpaceInteraction( Kassiopeia::KSSpaceInteraction* anInteraction );
-            void RemoveSpaceInteraction( Kassiopeia::KSSpaceInteraction* anInteraction );
-
-        private:
-            Kassiopeia::KSList< Kassiopeia::KSSpaceInteraction > fSpaceInteractions;
-            Kassiopeia::KSSpaceInteraction* fSpaceInteraction;
-
-            //******
-            //action
-            //******
-
-        public:
-            void SetStep( Kassiopeia::KSStep* anStep );
-            void SetTrajectory( Kassiopeia::KSTrajectory* aTrajectory );
-
-            void CalculateInteraction();
-            void ExecuteInteraction();
-
-            virtual void PushUpdateComponent();
-            virtual void PushDeupdateComponent();
-
-        private:
-            Kassiopeia::KSStep* fStep;
-            const Kassiopeia::KSParticle* fTerminatorParticle;
-            const Kassiopeia::KSParticle* fTrajectoryParticle;
-            Kassiopeia::KSParticle* fInteractionParticle;
-            Kassiopeia::KSParticle* fFinalParticle;
-            Kassiopeia::KSParticleQueue* fParticleQueue;
-            Kassiopeia::KSTrajectory* fTrajectory;
 
             kl_interface_ptr_t fInterface;
     };
