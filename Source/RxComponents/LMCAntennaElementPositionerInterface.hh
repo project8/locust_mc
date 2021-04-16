1d72718c155e4c6946814af9479cdcbb8d435e76
Penny Slocum <penny.slocum@yale.edu>
2021-04-15 21:18:43 -0400

    Consolidated included header files into *Interface*.hh files containing parent and derived class header files.  This reduces the number of include statements listed in LMCGenerator classes.

---
+++ Source/RxComponents/LMCAntennaElementPositionerInterface.hh
@@ -0,0 +1,16 @@
+/*
+ * LMCAntennaElementPositionerInterface.hh
+ *
+ *  Created on: Apr 15, 2021
+ *      Author: pslocum
+ */
+
+#ifndef LMCANTENNAELEMENTPOSITIONERINTERFACE_HH_
+#define LMCANTENNAELEMENTPOSITIONERINTERFACE_HH_
+
+#include "LMCAntennaElementPositioner.hh"
+#include "LMCSinglePatchPositioner.hh"
+#include "LMCPlanarArrayPositioner.hh"
+
+#endif
+
