d4830423e1a0cc7719efc8569b2c8b851c1a352d
Penny Slocum <penny.slocum@yale.edu>
2021-04-21 16:58:06 -0400

    Reading and parsing json file with Scarab v2, but not all parameters are being received yet by LMCRunLengthCalculator.

--- Source/Applications/LocustSim.cc
+++ Source/Applications/LocustSim.cc
@@ -5,8 +5,13 @@
  *      Author: nsoblath
  */
 
-#include "application.hh"
-#include "configurator.hh"
+//#include "application.hh"
+
+#include "param.hh"
+#include "param_json.hh"
+
+
+//#include "configurator.hh"
 #include "LMCException.hh"
 #include "LMCGeneratorToolbox.hh"
 #include "LMCSimulationController.hh"
@@ -21,9 +26,6 @@ LOGGER( lmclog, "LocustSim" );
 int main( int argc, char** argv )
 {
 
-    main_app the_main;
-    CLI11_PARSE( the_main, argc, argv );
-/*
     try
     {
         LPROG( lmclog, "Welcome to Locust_MC\n\n" <<
@@ -37,24 +39,52 @@ int main( int argc, char** argv )
                 "\t\t|____|\\___/\\__|  \\_,_|/__/ \\__|_____|_|  |_|  \\___| \n" <<
                 "\t\t                              |_____|               \n");
 
-        scarab::configurator configurator( argc, argv );
+//        scarab::configurator configurator( argc, argv );
+        //read file
+        param_input_json t_input;
+        param_ptr_t t_param_location( t_input.read_file( argv[1] ) );
+        if( ! t_param_location )
+        {
+            LERROR( lmclog, "File did not read!" );
+            return -1;
+        }
+
+        LINFO( lmclog, "File read and parsed: \n" << *t_param_location );
+
+        //write file
+        param_output_json t_output;
+        bool t_did_write_file = t_output.write_file( *t_param_location, "test_output.json" );
+
+        if( ! t_did_write_file )
+        {
+            LERROR( lmclog, "File did not write!" );
+            return -1;
+        }
+
+        LINFO( lmclog, "File written successfully (test_output.json)" );
 
         LPROG( lmclog, "Setting up generator toolbox" );
         GeneratorToolbox toolbox;
-        if( ! toolbox.Configure( configurator.config() ) )
+//        if( ! toolbox.Configure( configurator.config() ) )
+        if( ! toolbox.Configure( t_param_location->as_node() ) )
         {
             LERROR( lmclog, "Unable to configure the generator toolbox" );
             return -1;
         }
 
+
         LPROG( lmclog, "Setting up simulation controller" );
         SimulationController controller;
         controller.SetFirstGenerator( toolbox.GetFirstGenerator() );
-        if( ! controller.Configure( configurator.config()[ "simulation"].as_node()  ) )
+        //        if( ! controller.Configure( configurator.config()[ "simulation"].as_node()  ) )
+
+        if( ! controller.Configure( t_param_location->as_node() ))
         {
             LERROR( lmclog, "Unable to configure the simulation controller" );
             return -1;
         }
+
+        LPROG( lmclog, "Successfully set up simulation controller" );
 
         LPROG( lmclog, "Preparing for run" );
         if( ! controller.Prepare() )
@@ -84,7 +114,7 @@ int main( int argc, char** argv )
         LERROR( lmclog, "Exception caught: " << e );
         return e;
     }
-*/
+
 
     return 0;
 }
