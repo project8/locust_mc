b70e1db02c69f59fc7011847791b2df8c2c5e5f6
Penny Slocum <penny.slocum@yale.edu>
2021-07-15 14:44:54 -0400

    fixed some indenting.

--- Source/Transmitters/LMCTransmitter.hh
+++ Source/Transmitters/LMCTransmitter.hh
@@ -45,12 +45,12 @@ namespace locust
 
             /// Initialize the FIR filter and the field estimator
             virtual bool InitializeTransmitter(){};
-	    LMCThreeVector GetFieldPoint(int index);
-	    virtual LMCThreeVector GetIncidentKVector(int index);
-	    double GetPropagationPhaseDelay(int index);
-	    double GetNPoints();
-	    double GetMeanofFieldPoints(int axis);
-	    LMCThreeVector GetMeanofFieldPoints();
+            LMCThreeVector GetFieldPoint(int index);
+            virtual LMCThreeVector GetIncidentKVector(int index);
+            double GetPropagationPhaseDelay(int index);
+            double GetNPoints();
+            double GetMeanofFieldPoints(int axis);
+            LMCThreeVector GetMeanofFieldPoints();
 
 	protected:
 
@@ -59,13 +59,13 @@ namespace locust
     	    virtual void AddPropagationPhaseDelay(LMCThreeVector fieldPoint);
     	    void SetFieldPoint(int index,LMCThreeVector fieldPoint);
     	    void SetIncidentKVector(int index,LMCThreeVector incidentKVector);
-	    void SetPropagationPhaseDelay(int index,double phaseDelay);
+    	    void SetPropagationPhaseDelay(int index,double phaseDelay);
 
-        private:
-	    std::vector< LMCThreeVector> fFieldPoints; 
-	    std::vector< LMCThreeVector> fIncidentKVectors;
-	    std::vector< double> fPropagationPhaseDelays;
-	    void AddFieldPoint(LMCThreeVector fieldPoint);
+    private:
+    	    std::vector< LMCThreeVector> fFieldPoints;
+    	    std::vector< LMCThreeVector> fIncidentKVectors;
+    	    std::vector< double> fPropagationPhaseDelays;
+    	    void AddFieldPoint(LMCThreeVector fieldPoint);
 };
 
 
