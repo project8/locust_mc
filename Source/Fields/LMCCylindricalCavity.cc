45766a2f3f5eb8c10d3ce2aa9d168de3bb5002be
Penny Slocum <penny.slocum@yale.edu>
2022-03-13 22:50:48 -0400

    Implement optional averaging over theta for radial component of ode field.  Averaging is used everywhere except in mode-maps.

--- Source/Fields/LMCCylindricalCavity.cc
+++ Source/Fields/LMCCylindricalCavity.cc
@@ -152,19 +152,21 @@ namespace locust
     	double eta = sqrt( LMCConst::MuNull() / LMCConst::EpsNull() );  // Pozar p. 291.
     	double jl_of_k1r_by_k1r = 1./(2.*l) * (boost::math::cyl_bessel_j(l-1, k1*r) + boost::math::cyl_bessel_j(l+1, k1*r));
     	double jPrime = 1./2. * boost::math::cyl_bessel_j(l-1, k1*r) - boost::math::cyl_bessel_j(l+1, k1*r);
-    	double tEr = -l * k/k1 * eta * jl_of_k1r_by_k1r * sin(l*theta) * sin(k3*z);
-    	TE_E.push_back(tEr);
+    	double tEr = 0.;
     	double tEtheta = 0.;
 
     	if ((!avgOverTheta)||(l==0))
     	{
+        	tEr = -l * k/k1 * eta * jl_of_k1r_by_k1r * sin(l*theta) * sin(k3*z);
     		tEtheta = -k/k1 * eta * jPrime * cos(l*theta) * sin(k3*z);
     	}
     	else
     	{
+        	tEr = -l * k/k1 * eta * jl_of_k1r_by_k1r * (2./LMCConst::Pi()) * sin(k3*z);
     		tEtheta = -k/k1 * eta * jPrime * (2./LMCConst::Pi()) * sin(k3*z);
     	}
 
+    	TE_E.push_back(tEr);
     	TE_E.push_back(tEtheta);
         return TE_E;
     }
@@ -182,21 +184,23 @@ namespace locust
     	double k = pow(k1*k1+k3*k3,0.5);
     	double jl_of_k1r_by_k1r = 1./(2.*l) * (boost::math::cyl_bessel_j(l-1, k1*r) + boost::math::cyl_bessel_j(l+1, k1*r));
     	double jPrime = 1./2. * boost::math::cyl_bessel_j(l-1, k1*r) - boost::math::cyl_bessel_j(l+1, k1*r);
-    	double tHr = -k3/k1 * jPrime * cos(l*theta) * cos(k3*z);
     	double tHz = boost::math::cyl_bessel_j(l, k1*r) * cos(l*theta) * sin(k3*z);
-    	TE_H.push_back(tHr);  // r
     	TE_H.push_back(tHz);  // z
+    	double tHr = 0.;
     	double tHtheta = 0.;
 
     	if ((!avgOverTheta)||(l==0))
     	{
+        	tHr = -k3/k1 * jPrime * cos(l*theta) * cos(k3*z);
     		tHtheta = -l*k3/k1 * jl_of_k1r_by_k1r * sin(l*theta) * cos(k3*z);
     	}
     	else
     	{
+        	tHr = -k3/k1 * jPrime * (2./LMCConst::Pi()) * cos(k3*z);
     		tHtheta = -l*k3/k1 * jl_of_k1r_by_k1r * (2./LMCConst::Pi()) * cos(k3*z);
     	}
 
+    	TE_H.push_back(tHr);  // r
     	TE_H.push_back(tHtheta); // theta
     	return TE_H; // r, z, theta
     }
@@ -214,21 +218,23 @@ namespace locust
     	double eta = sqrt( LMCConst::MuNull() / LMCConst::EpsNull() );  // Pozar p. 291.
     	double jl_of_k1r_by_k1r = 1./(2.*l) * (boost::math::cyl_bessel_j(l-1, k1*r) + boost::math::cyl_bessel_j(l+1, k1*r));
     	double jPrime = 1./2. * boost::math::cyl_bessel_j(l-1, k1*r) - boost::math::cyl_bessel_j(l+1, k1*r);
-    	double tEr = -k3/k1 * eta * jPrime * cos(l*theta) * cos(k3*z);
     	double tEz = eta * boost::math::cyl_bessel_j(l, k1*r) * cos(l*theta) * sin(k3*z);
-    	TM_E.push_back(tEr); // r
     	TM_E.push_back(tEz);  // z
+    	double tEr = 0.;
     	double tEtheta = 0.;
 
     	if ((!avgOverTheta)||(l==0))
     	{
+        	tEr = -k3/k1 * eta * jPrime * cos(l*theta) * cos(k3*z);
     		tEtheta = -l*k3/k1 * eta * jl_of_k1r_by_k1r * sin(l*theta) * cos(k3*z);
     	}
     	else
     	{
+        	tEr = -k3/k1 * eta * jPrime * (2./LMCConst::Pi()) * cos(k3*z);
     		tEtheta = -l*k3/k1 * eta * jl_of_k1r_by_k1r * (2./LMCConst::Pi()) * cos(k3*z);
     	}
 
+    	TM_E.push_back(tEr); // r
     	TM_E.push_back(tEtheta);  // theta
     	return TM_E; // r, z, theta
     }
@@ -245,19 +251,21 @@ namespace locust
     	double k = pow(k1*k1+k3*k3,0.5);
     	double jl_of_k1r_by_k1r = 1./(2.*l) * (boost::math::cyl_bessel_j(l-1, k1*r) + boost::math::cyl_bessel_j(l+1, k1*r));
     	double jPrime = 1./2. * boost::math::cyl_bessel_j(l-1, k1*r) - boost::math::cyl_bessel_j(l+1, k1*r);
-    	double tHr = -l * k/k1  * jl_of_k1r_by_k1r * sin(l*theta) * sin(k3*z);
-    	TM_H.push_back(tHr);  // r
+    	double tHr = 0.;
     	double tHtheta = 0.;
 
     	if ((!avgOverTheta)||(l==0))
     	{
+        	tHr = -l * k/k1  * jl_of_k1r_by_k1r * sin(l*theta) * sin(k3*z);
     		tHtheta = -k/k1 * jPrime * cos(l*theta) * sin(k3*z);
     	}
     	else
     	{
+        	tHr = -l * k/k1  * jl_of_k1r_by_k1r * (2./LMCConst::Pi()) * sin(k3*z);
     		tHtheta = -k/k1 * jPrime * (2./LMCConst::Pi()) * sin(k3*z);
     	}
 
+    	TM_H.push_back(tHr);  // r
     	TM_H.push_back(tHtheta); // theta
         return TM_H;
     }
