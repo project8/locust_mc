25faa18981c10afd71ec33d0c6f9abf0ed8f5df1
Penny Slocum <penny.slocum@yale.edu>
2022-07-25 21:19:18 -0400

    Power combining in LMCCavityModes is now the same as the e-gun, as in LMCWaveguideModes.  This is possible due to the convolution with complex Green's function.  The FSCD analog is to drive the antenna with a signal that has complex magnitude and phase.

--- Source/RxComponents/LMCCavityModes.cc
+++ Source/RxComponents/LMCCavityModes.cc
@@ -70,12 +70,11 @@ namespace locust
 	{
 		double dopplerFrequency = cavityDopplerFrequency[0];
         SetVoltagePhase( GetVoltagePhase() + dopplerFrequency * dt ) ;
-        fOrbitPhase += particleXP[7] * dt;
-        double phaseLag = GetVoltagePhase() - fOrbitPhase;
         double voltageValue = excitationAmplitude * EFieldAtProbe * fProbeGain;
+        voltageValue *= cos(GetVoltagePhase());
 
-        aSignal->LongSignalTimeComplex()[sampleIndex][0] += 2. * voltageValue * totalScalingFactor * sin(phi_LO - phaseLag);
-        aSignal->LongSignalTimeComplex()[sampleIndex][1] += 2. * voltageValue * totalScalingFactor * cos(phi_LO - phaseLag);
+        aSignal->LongSignalTimeComplex()[sampleIndex][0] += 2. * voltageValue * totalScalingFactor * sin(phi_LO);
+        aSignal->LongSignalTimeComplex()[sampleIndex][1] += 2. * voltageValue * totalScalingFactor * cos(phi_LO);
 
 		if ( GetVoltageCheck() && (sampleIndex%100 < 1) )
 			LPROG( lmclog, "Voltage " << sampleIndex << " is <" << aSignal->LongSignalTimeComplex()[sampleIndex][1] << ">" );
