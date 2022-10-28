de20019e34d80ddab6322cb53de79a56d61a86ef
Penny Slocum <penny.slocum@yale.edu>
2022-10-28 13:47:26 -0400

    Update interface to remove AnalyticResponseFunction references and add new LMCConfigureKass class to pass Locust config parameters to Kass.

--- Source/Kassiopeia/LMCKassLocustInterface.cc
+++ Source/Kassiopeia/LMCKassLocustInterface.cc
@@ -34,18 +34,12 @@ namespace locust
             fProject8Phase( 0 ),
             fCENTER_TO_SHORT( 0.05 ),
             fCENTER_TO_ANTENNA( 0.05 ),
-			fTFReceiverHandler(),
-			fTransmitter(),
-			fAnalyticResponseFunction(),
-			fBesselNKZeros( 0 ),
-			fBesselNKPrimeZeros( 0 ),
-			dtFilter( 0 ),
-			fField(),
-			dotProductFactor( 0. ),
-			modeAmplitude( 0. ),
-			CavityFIRSample( 0. ),
-			fBackReaction( false ),
-			fE_Gun( false )
+            fTransmitter(),
+            fBesselNKZeros( 0 ),
+            fBesselNKPrimeZeros( 0 ),
+            fField(),
+            fBackReaction( false ),
+            fE_Gun( false )
 
     {}
 
