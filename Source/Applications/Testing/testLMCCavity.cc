07993fd1bcfcaf86f5a009280d705dc57afe6e38
Penny Slocum <penny.slocum@yale.edu>
2023-01-23 20:55:42 -0500

    Update parameter definitions and availability.

--- Source/Applications/Testing/testLMCCavity.cc
+++ Source/Applications/Testing/testLMCCavity.cc
@@ -16,25 +16,47 @@ using namespace locust;
 
 LOGGER( testlog, "testLMCCavity" );
 
-class test_app : public main_app
+class testCavity_app : public main_app
 {
     public:
-        test_app() :
+        testCavity_app() :
             main_app(),
-			fTestParameter(0.)
+			fDHOTimeResolution(1.e-8),
+			fDHOThresholdFactor(0.01),
+			fCavityFrequency(1.067e9),
+			fCavityQ(1000.)
         {
-            add_option("-t,--test-parameter", fTestParameter, "Set a test parameter." );
+            add_option("-r,--dho-time-resolution", fDHOTimeResolution, "Time resolution used in Green's function (s).");
+            add_option("-m,--dho-threshold-factor", fDHOThresholdFactor, "Minimum fractional threshold of Green's function used to calculate FIR.");
+            add_option("-f,--dho-cavity-frequency", fCavityFrequency, "Cavity resonant frequency (Hz).");
+            add_option("-q,--dho-cavity-Q", fCavityQ, "Cavity Q.");
         }
 
-        virtual ~test_app() {}
+        virtual ~testCavity_app() {}
 
-        double GetTestParameter()
+        double GetDHOTimeResolution()
         {
-        	return fTestParameter;
+        	return fDHOTimeResolution;
         }
+        double GetDHOThresholdFactor()
+        {
+        	return fDHOThresholdFactor;
+        }
+        double GetCavityFrequency()
+        {
+        	return fCavityFrequency;
+        }
+        double GetCavityQ()
+        {
+        	return fCavityQ;
+        }
+
 
     private:
-        double fTestParameter;
+        double fDHOTimeResolution;
+        double fDHOThresholdFactor;
+        double fCavityFrequency;
+        double fCavityQ;
 };
 
 
@@ -44,19 +66,25 @@ public:
 
 	testLMCCavity():
         fRF_frequency(0.),
-        fLO_frequency(0.),
-        fAmplitude(0.),
         fAcquisitionRate(0.),
 		fFilterRate(0.),
-		fPhaseLagLO(0.),
 		fTFReceiverHandler( 0 ),
-		fAnalyticResponseFunction( 0 )
+		fAnalyticResponseFunction( 0 ),
+	    param_0( new param_node() )
     {
     }
 
+	void AddParam(std::string aString, double aValue)
+	{
+		if ( aValue != 0. )
+		{
+            param_0->add(aString.c_str(), aValue);
+		}
+	}
+
 	const scarab::param_node* GetParams()
 	{
-	    scarab::param_node* param_0( new param_node() );
+		testCavity_app the_main;
 	    std::string tDataDir = TOSTRING(PB_DATA_INSTALL_DIR);
 	    std::string filepath = tDataDir + "/PatchTFLocust.txt";
 	    param_0->add( "tf-receiver-filename", filepath.c_str() );
@@ -85,11 +113,8 @@ public:
 			return false;
 		}
 
-        fRF_frequency = 0.9e9; // Hz
-        fLO_frequency = 0.05e9; // Hz
+        fRF_frequency = 0.; // Hz
         fAcquisitionRate = 201.e6; // Hz
-        fAmplitude = 5.e-6; // volts
-        fPhaseLagLO = 0.;
 		return true;
 	}
 
@@ -98,22 +123,16 @@ public:
 	bool PopulateSignal(Signal* aSignal, int N0, double timeStamp)
 	{
 
-        double LO_phase = 0.;
         double voltage_phase = 0.;
         double phaseLagRF = 2.*LMCConst::Pi() * timeStamp/(1./fRF_frequency);
-        fPhaseLagLO += 2.*LMCConst::Pi() * fLO_frequency * 1./fAcquisitionRate;
 
         for( unsigned index = 0; index < N0; ++index )
         {
-            LO_phase = fPhaseLagLO;
             voltage_phase = phaseLagRF + 2.*LMCConst::Pi()*fRF_frequency*(double)index/fFilterRate;
 
-            // keep only lower sideband RF-LO
-            aSignal->LongSignalTimeComplex()[index][0] = fAmplitude*cos(voltage_phase-LO_phase);
-            aSignal->LongSignalTimeComplex()[index][1] = fAmplitude*cos(-LMCConst::Pi()/2. + voltage_phase-LO_phase);
+            aSignal->LongSignalTimeComplex()[index][0] = cos(voltage_phase);
+            aSignal->LongSignalTimeComplex()[index][1] = cos(-LMCConst::Pi()/2. + voltage_phase);
         }
-//        printf("fAmplitude is %g, signal[0][0] is %g with phaseLagRF %g and LOPhase %g at time stamp %g\n", fAmplitude, aSignal->LongSignalTimeComplex()[0][0], phaseLagRF, fPhaseLagLO, timeStamp);
-//        getchar();
         return true;
 	}
 
@@ -130,20 +149,17 @@ public:
 
     TFReceiverHandler* fTFReceiverHandler;
     AnalyticResponseFunction* fAnalyticResponseFunction;
+    scarab::param_node* param_0;
+
 
     double fRF_frequency;
-    double fLO_frequency;
     double fAcquisitionRate;
     double fFilterRate;
-    double fAmplitude;
-    double fPhaseLagLO;
-
-
 
 };
 
 
-int parseCavity(test_app& the_main)
+int parseCavity(testCavity_app& the_main)
 {
 	TestParameterHandler* p1 = TestParameterHandler::getInstance();
     CLI11_PARSE( the_main, p1->GetArgc(), p1->GetArgv() );
@@ -153,11 +169,15 @@ int parseCavity(test_app& the_main)
 
 TEST_CASE( "testLMCCavity with default parameter values (pass)", "[single-file]" )
 {
-	test_app the_main;
+	testCavity_app the_main;
 	parseCavity(the_main);
-	LPROG( testlog, "fTestParameter is " << the_main.GetTestParameter() );
 
 	testLMCCavity aTestLMCCavity;
+	aTestLMCCavity.AddParam( "dho-time-resolution", the_main.GetDHOTimeResolution() );
+	aTestLMCCavity.AddParam( "dho-threshold-factor", the_main.GetDHOThresholdFactor() );
+	aTestLMCCavity.AddParam( "dho-cavity-frequency", the_main.GetCavityFrequency() );
+	aTestLMCCavity.AddParam( "dho-cavity-Q", the_main.GetCavityQ() );
+
 	if (!aTestLMCCavity.Configure())
 	{
 		LWARN(testlog,"testLMCCavity was not configured correctly.");
@@ -170,11 +190,15 @@ TEST_CASE( "testLMCCavity with default parameter values (pass)", "[single-file]"
     aTestLMCCavity.fFilterRate = (1./aTestLMCCavity.fTFReceiverHandler->GetFilterResolution());
     aSignal->Initialize( N0 , 1 );
 
-    double firGainMax = 0.;
+    double qInferred = 0.;
+    double maxGain = 0.;
+    double rfSpanSweep = 3. * the_main.GetCavityFrequency() / the_main.GetCavityQ();
+    double rfStepSize = 0.00005 * the_main.GetCavityFrequency();
+    int nSteps = rfSpanSweep / rfStepSize;
 
-        for (unsigned rfStep=0; rfStep<30; rfStep++) // frequency sweep
+        for (int rfStep=-nSteps/2; rfStep<nSteps/2; rfStep++) // frequency sweep
         {
-            aTestLMCCavity.fRF_frequency = 1.0e9 + 0.005e9*rfStep;
+            aTestLMCCavity.fRF_frequency = the_main.GetCavityFrequency() + rfStepSize * rfStep;
 
 	        double convolutionMag = 0.;
             for (unsigned i=0; i<1000; i++)  // time stamps
@@ -188,13 +212,23 @@ TEST_CASE( "testLMCCavity with default parameter values (pass)", "[single-file]"
         	        convolutionMag = convolutionPair.first;
                 }
             } // i
-            double firGain = convolutionMag;
-			LPROG( testlog, "Cavity GF gain at frequency " << aTestLMCCavity.fRF_frequency << " is " << firGain );
+
+            if (convolutionMag*convolutionMag > maxGain)
+            {
+            	maxGain = convolutionMag*convolutionMag;
+            }
+            else if ((convolutionMag*convolutionMag < 0.5*maxGain) && (qInferred == 0.))
+            {
+            	qInferred = the_main.GetCavityFrequency() /  (2.* rfStepSize * (rfStep-1));
+            }
+			LPROG( testlog, "Cavity GF gain at frequency " << aTestLMCCavity.fRF_frequency << " is " << convolutionMag );
         } // rfStep
 
     delete aSignal;
 
-    REQUIRE( 1 > 0. ); // to-do:  get this defined.
+    LPROG( testlog, "Estimated Q is " << qInferred );
+    LPROG( testlog, "Expected Q is " << the_main.GetCavityQ() );
+    REQUIRE( qInferred > 0. );
 }
 
 
