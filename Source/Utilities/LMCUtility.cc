3b4146ec43ab00b8a106248de3df6cb8eeafef7e
Penny Slocum <penny.slocum@yale.edu>
2023-02-12 21:09:49 -0500

    Move unit test calculations into Utilities/ directory for easier access with both simulation and unit testing.

---
+++ Source/Utilities/LMCUtility.cc
@@ -0,0 +1,19 @@
+/*
+ * LMCUtility.cc
+ *
+ *  Created on: Feb 10, 2023
+ *      Author: pslocum
+ */
+
+#include "LMCUtility.hh"
+
+
+namespace locust
+{
+    LOGGER( lmclog, "Utility" );
+    Utility::Utility() {}
+    Utility::~Utility() {}
+
+
+} /* namespace locust */
+
