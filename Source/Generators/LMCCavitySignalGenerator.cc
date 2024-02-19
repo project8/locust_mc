4cc3ab90857386d9d4bddbd00da48dc8e736e323
Penny Slocum <penny.slocum@yale.edu>
2024-02-19 08:39:07 -0500

    Randomize start times for each track during pileup runs.

--- Source/Generators/LMCCavitySignalGenerator.cc
+++ Source/Generators/LMCCavitySignalGenerator.cc
@@ -266,6 +266,8 @@ namespace locust
         	    }
         	    else
         	    {
+        	    	// Delay SetSeed to allow time stamp to advance between randomized tracks.
+            		std::this_thread::sleep_for(std::chrono::milliseconds(2000));
         	    	SetSeed (time(NULL) );
         	    }
             }
