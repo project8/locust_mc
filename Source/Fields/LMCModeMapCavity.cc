5083b1d08175f5a572eacae903c2f9c4e33f1fd1
Penny Slocum <penny.slocum@yale.edu>
2024-11-24 21:15:38 -0500

    Fix some indenting.

--- Source/Fields/LMCModeMapCavity.cc
+++ Source/Fields/LMCModeMapCavity.cc
@@ -116,7 +116,7 @@ namespace locust
                 std::string token;
                 std::stringstream ss(lineContent);
                 int wordCount = 0;
-		double r, theta, z;
+                double r, theta, z;
                 int i,j,k;
                 double Erho,Etheta;
                 double Ex, Ey, Ez;
@@ -124,19 +124,19 @@ namespace locust
                 {
                     if (wordCount == 0)
                     {
-			r = std::stod(token);
+                        r = std::stod(token);
                         i = (int)((r-fDim1_min)/(fDim1_max-fDim1_min)*(fnPixel1)); // var1 position
-		    }
+                    }
                     else if (wordCount == 1) 
-		    {
-			theta = std::stod(token);
-			j = (int)((theta-fDim2_min)/(fDim2_max-fDim2_min)*(fnPixel2)); // var2 position
-		    }
+                    {
+                        theta = std::stod(token);
+                        j = (int)((theta-fDim2_min)/(fDim2_max-fDim2_min)*(fnPixel2)); // var2 position
+                    }
                     else if (wordCount == 2)
-		    {
-			z = std::stod(token);
-			k = (int)((z-fDim3_min)/(fDim3_max-fDim3_min)*(fnPixel3)); // var3 position
-		    }
+                    {
+                        z = std::stod(token);
+                        k = (int)((z-fDim3_min)/(fDim3_max-fDim3_min)*(fnPixel3)); // var3 position
+                    }
                     else if (wordCount == 3) Ex = std::stod(token); // mode E field value
                     else if (wordCount == 4) Ey = std::stod(token); // mode E field value
                     else if (wordCount == 5) Ez = std::stod(token); // mode E field value
@@ -148,10 +148,10 @@ namespace locust
                     ++wordCount;
                 }
 
-		if ((i==fnPixel1) or (j==fnPixel2) or (k==fnPixel3))
-		{
-			continue;
-		}
+                if ((i==fnPixel1) or (j==fnPixel2) or (k==fnPixel3))
+                {
+                    continue;
+                }
                 if ((i>=fnPixel1) or (j>=fnPixel2) or (k>=fnPixel3))
                 {   
                     LERROR(lmclog,"Imported mode map dimensions don't agree with those in \"" << aFilename <<".\" Double check dim[1,2,3]-max.");
@@ -159,26 +159,27 @@ namespace locust
                 }   
 
                 //Must convert E field from cartesian coordinates to cylindrical coordinates
-		if(r<1.e-10)
-		{
-		    Erho = 0.;
-		    Etheta = 0.;
-		}
-		else
-		{
+                if(r<1.e-10)
+                {
+                    Erho = 0.;
+                    Etheta = 0.;
+                }
+                else
+                {
                     Erho = ((Ex * r*cos(theta)) + Ey * r*sin(theta)) / r;
-		    Etheta = ((Ey * r*cos(theta)) - Ex * r*sin(theta)) / r;
-		}
+                    Etheta = ((Ey * r*cos(theta)) - Ex * r*sin(theta)) / r;
+                }
 
                 std::vector E_input = {Erho,Etheta,Ez};
                 fModeMapTE_E[i][j][k] = E_input;
-//              printf("read var1 is %g, var2 is %g, E is %g\n", fModeMapTE_E.back()[0], fModeMapTE_E.back()[1], fModeMapTE_E.back()[2]);
+                //printf("read var1 is %g, var2 is %g, E is %g\n", fModeMapTE_E.back()[0], fModeMapTE_E.back()[1], fModeMapTE_E.back()[2]);
+
             }
         }
 
         modeMapFile.close();
 
-	//Reset dimensions from import file to actual cavity dimensions in case they don't match up
+        //Reset dimensions from import file to actual cavity dimensions in case they don't match up
         MatchCavityDimensions(aParam);
 
         return true;
