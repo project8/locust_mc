6ed73c546b62fe8142f0de7fec953df98205a88f
muelenator <rickymu97@gmail.com>
2025-07-11 14:05:33 -0400

    Edits on previous

--- Source/RxComponents/LMCDampedHarmonicOscillator.cc
+++ Source/RxComponents/LMCDampedHarmonicOscillator.cc
@@ -59,6 +59,7 @@ namespace locust
                 } else {
                     LERROR(lmclog, "Invalid fraction format for dho-time-resolution: " << fraction);
                 }
+                LPROG("DHO time res with dipole=False: " << GetDHOTimeResolution());
             } else {
                 SetDHOTimeResolution(timeResolutionParam.as_double());
             }
