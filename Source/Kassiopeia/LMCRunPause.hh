d61c1b6a0254355bad6b9cf4633fc79de24c2cb1
Penny Slocum <penny.slocum@yale.edu>
2025-09-12 10:04:28 -0400

    Update Kass object handling in LMCRunPause.hh.

---
+++ Source/Kassiopeia/LMCRunPause.hh
@@ -0,0 +1,128 @@
+/*
+ * LMCRunPause.hh
+ *
+ *  Created on: Jul 31, 2019
+ *      Author: N.S. Oblath
+ */
+
+#ifndef LOCUST_LMCRUNPAUSE_HH_
+#define LOCUST_LMCRUNPAUSE_HH_
+
+#include "KSRunModifier.h"
+#include "KToolbox.h"
+#include "KSTermMaxR.h"
+#include "KSTermMaxEnergy.h"
+#include "KSTermMinEnergy.h"
+#include "KSTermMaxTime.h"
+#include "KSTermDeath.h"
+#include "KSRootTerminator.h"
+#include "KSGeoSpace.h"
+#include "KSGeoSurface.h"
+#include "KGBoxSpace.hh"
+#include "KGCore.hh"
+#include "KSRootGenerator.h"
+#include "KSGenGeneratorComposite.h"
+#include "KSGenValueUniform.h"
+#include "KSGenValueAngleSpherical.h"
+#include "KSGenValueRadiusCylindrical.h"
+#include "KSGenValueFix.h"
+#include "KSGenEnergyComposite.h"
+#include "KSGenEnergyKryptonEvent.h"
+#include "KSGenPositionRectangularComposite.h"
+#include "KSGenPositionCylindricalComposite.h"
+#include "KSGenDirectionSphericalComposite.h"
+#include "KSGenTimeComposite.h"
+#include "LMCKassLocustInterface.hh"
+#include "KRandom.h"
+#include "LMCDistributionInterface.hh"
+#include <stdlib.h>
+#include <time.h>
+
+
+namespace locust
+{
+
+    class RunPause :
+            public Kassiopeia::KSComponentTemplate< RunPause, Kassiopeia::KSRunModifier >
+    {
+        public:
+            RunPause();
+            RunPause( const RunPause& aOrig );
+            virtual ~RunPause();
+
+            RunPause* Clone() const;
+
+        public:
+
+            virtual bool ExecutePreRunModification( Kassiopeia::KSRun& aRun );
+            virtual bool ExecutePostRunModification( Kassiopeia::KSRun& aRun );
+
+            bool ConfigureByInterface();
+            bool Configure( const scarab::param_node& aParam );
+            Kassiopeia::KSGeoSpace* GetKSWorldSpace();
+            KGeoBag::KGSpace* GetKGWorldSpace();
+            bool DeleteLocalKassObjects();
+            bool AddWaveguideTerminator( const scarab::param_node& aParam );
+            bool AddMaxTimeTerminator( const scarab::param_node& aParam );
+            bool AddEnergyTerminators( const scarab::param_node& aParam );
+            bool AddMaxRTerminator( const scarab::param_node& aParam );
+            bool AddGenerator( const scarab::param_node& aParam );
+            bool HaveStartingPositions( const scarab::param_node& aParam );
+            bool HaveGenConfigParams( const scarab::param_node& aParam );
+            bool SetupGenerator( const scarab::param_node& aParam );
+            bool ConfigureUniformGenerator( const scarab::param_node& aParam );
+            bool SetupDirection( const scarab::param_node& aParam );
+            long int GetSeed( const scarab::param_node& aParam );
+
+
+
+
+        protected:
+            kl_interface_ptr_t fInterface;
+
+        private:
+            katrin::KToolbox& fToolbox;
+            Kassiopeia::KSTermMaxTime* fLocustMaxTimeTerminator;
+            Kassiopeia::KSTermMaxR* fLocustMaxRTerminator;
+            Kassiopeia::KSTermMaxEnergy* fLocustMaxEnergyTerminator;
+            Kassiopeia::KSTermMinEnergy* fLocustMinEnergyTerminator;
+            KGeoBag::KGBoxSpace* fBox;
+            Kassiopeia::KSGeoSurface* fSurface;
+            Kassiopeia::KSTermDeath* fLocustTermDeath;
+            Kassiopeia::KSCommand* fCommand;
+            Kassiopeia::KSGenGeneratorComposite* fGenerator;
+            Kassiopeia::KSGenDirectionSphericalComposite* fGenDirectionComposite;
+            Kassiopeia::KSGenValueAngleSpherical* fThetaGenerator; // pitch angle
+            Kassiopeia::KSGenValueUniform* fPhiGenerator; // orbit phase
+            Kassiopeia::KSGenPositionRectangularComposite* fGenPositionRectangularComposite;
+            Kassiopeia::KSGenPositionCylindricalComposite* fGenPositionCylindricalComposite;
+            Kassiopeia::KSGenValueUniform* fPositionPhiGenerator;
+            Kassiopeia::KSGenValueRadiusCylindrical* fPositionRadiusGenerator;
+            Kassiopeia::KSGenValueUniform* fPositionXGenerator;
+            Kassiopeia::KSGenValueUniform* fPositionYGenerator;
+            Kassiopeia::KSGenValueUniform* fPositionZGenerator;
+            Kassiopeia::KSGenValueUniform* fEnergyUniform;
+            Kassiopeia::KSGenEnergyKryptonEvent* fEnergyKrypton;
+            Kassiopeia::KSGenEnergyComposite* fGenEnergyComposite;
+            Kassiopeia::KSGenCreator* fGenEnergyCreator;
+            Kassiopeia::KSGenTimeComposite* fGenTimeComposite;
+            Kassiopeia::KSGenValueUniform* fTimeGenerator;
+            Kassiopeia::KSGenValueFix* fGenPidComposite;
+
+            std::shared_ptr< BaseDistribution> fTrackLengthDistribution;
+            DistributionInterface fDistributionInterface;
+            double fMinTrackLengthFraction;
+            bool fConfigurationComplete;
+            int fEventCounter;
+            int fMaxEvents;
+
+
+
+
+
+    };
+
+} /* namespace locust */
+
+#endif /* LOCUST_LMCRUNPAUSE_HH_ */
+
